fb60817ce78343c521e4d64137b49942a3035da7
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2023-09-06 23:42:29 -0600

    main.js fix

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import { randomNumbers, checkEnd, renderBoard, updateBoard } from "./js/game.js";
+import { renderBoard, updateBoard } from "./js/game.js";
 import { board, finalState, square } from "./js/const.js";
 
 const game = document.querySelector('.game');
 const modal = document.querySelector('.modal')
 const modalBtn = document.getElementById('modal-btn');
 const boxes = document.querySelectorAll('.box');
+const startBtn = document.querySelector('.start-btn');
 
 let counter = 0;
 function initializeState() {
@@ -19,25 +20,32 @@ function initializeState() {
     })
   })
 }
-
 function checkValid(boxes) {
   boxes.forEach(box => {
     if (box.textContent == '') return false
   })
   return true
 }
+renderBoard(board, game, square)
+initializeState();
 
 modalBtn.addEventListener('click', () => {
   if (checkValid(boxes)) {
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-
-      }
-    }
+    boxes.forEach((box, index) => {
+      const row = Math.floor(index / 2);
+      const column = index % 2;
+      board[row][column] = box.textContent;
+    })
   }
+  modal.classList.add('hidden')
+  renderBoard(board, game, square)
 })
 
-renderBoard(board, game, square)
+startBtn.addEventListener('click', () => {
+  updateBoard(counter, board, finalState, game, square).then(result => {
+
+  })
+})
 
 /* updateBoard().then(result => {
   console.log("Numero de repeticiones: ", result)
